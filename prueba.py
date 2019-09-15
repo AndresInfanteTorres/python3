20ebcac4e5c444b94a794ed7623178c1cbbc8d61
unknown <andres.infante@live.com>
2019-09-15 02:41:59 -0500

    adding prueba.py file with some exeicises

---
+++ prueba.py
@@ -0,0 +1,49 @@
+class andresClass:
+    def imprimir(a,b):
+        print (a+b)
+    
+
+    
+    def numero_perfecto(num):
+        ciclos=num/2
+        numerosSumar = []
+        for i in range (1,(int(ciclos))+1):
+            if (num%i)==0:
+                numerosSumar.append(i)
+        suma=0
+        for x in numerosSumar:
+            suma=suma+x
+            
+        print (numerosSumar)
+        if (num==suma):
+            print ("numero perfecto")
+        else:
+            print ("numero imperfecto")
+
+    def most_frecuent(given_array):
+        print ("most frecuent function")
+        max=0
+        new=0
+        for x in given_array:
+            band=0
+            for y in given_array:
+                if (x==y):
+                    band=band+1
+            if(band>max):
+                new=x
+                max=band
+                
+       
+        print ("The most frecuent number is ",new, " it repits ",max, " times.")                               
+            
+                
+    array=[1,3,4,3,5,6,4,5,6,3,4576,45,776,2,4,21,2,2,5,67,2]         
+    imprimir(5,45)
+    numero_perfecto(6)
+    numero_perfecto(496)
+    numero_perfecto(8128)
+    numero_perfecto(8127)
+    numero_perfecto(77)
+    numero_perfecto(676)
+    most_frecuent(array)
+
