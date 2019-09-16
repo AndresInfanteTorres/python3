6cc166d39fe480a97b780f6118a9c799bcd16ad1
unknown <andres.infante@live.com>
2019-09-15 23:44:24 -0500

    adding fibonacci function

---
+++ fibonacci.py
@@ -0,0 +1,24 @@
+#fibonacci method
+def fibonacci():
+    
+    for i in range (0,10):
+        print (i)
+    a=0
+    b=1
+    aux =0
+    print ("fibonacci 1")
+    for i in range (0,10):
+        print (a)
+        aux = a
+        a=a+b
+        b=aux
+
+def fibonacci2():
+    print ("fibonacci 2")
+    a=0
+    b=1
+    for i in range (0,10):
+        print ("Try number",i+1,a)
+        a,b=b,a+b
+fibonacci()
+fibonacci2()
