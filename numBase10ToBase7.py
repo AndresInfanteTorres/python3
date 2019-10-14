05184251a7eb2a4da58f2555913bbfef552481da
AndresInfanteTorres <andres.infante@live.com>
2019-10-14 00:25:09 -0500

    adding a function to convert a num base 10 to base 7

---
+++ numBase10ToBase7.py
@@ -0,0 +1,15 @@
+#write a function that recives a num base 10 and change it to base 7 and returns the number as a string
+def  base10ToBase7(num):
+    res=[]
+    while (num >7):
+        res.append(num % 7)
+        num=int(num/7)
+        if (num<7):
+            res.append(num)
+    tam=len(res)-1
+    word=''
+    while(tam>=0):
+        word=word+str(res[tam])
+        tam=tam-1
+    return word
+print (base10ToBase7(123))
